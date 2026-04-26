c00d818942538cad564542156b1cf6adfcc91a58
xujunmeng <xujunmeng2012@163.com>
2026-04-26 19:43:47 +0800

    update

--- search_crawler/config.py
+++ search_crawler/config.py
@@ -19,7 +19,7 @@ CRAWL_CONFIG = {
     'page_size': 20,                    # 每页数量
     
     # 定时调度配置（统一在此配置）
-    'schedule_interval': 600,          # 定时间隔(秒)，默认1小时=3600秒
+    'schedule_interval': 3600,          # 定时间隔(秒)，默认1小时=3600秒
     'schedule_cron': '*/10',            # Cron表达式（分钟位），*/10表示每10分钟执行一次
     
     # APScheduler 容错配置（统一从CRAWL_CONFIG读取）
@@ -47,7 +47,7 @@ COOKIE_POOL_CONFIG = {
 COOKIE_POOL = [
     {
         'name': 'account_01',
-        'value': 'abRequestId=7e3310df-011a-5d42-943e-fb44df97212c; a1=19c94f738easkxvhhq9y1cv91oxe8lxj119x1sxe130000383551; webId=f769b9d526c8861d4325189df5578d03; gid=yjSj4iW4WdyYyjSj4iWqYyMCd0MTChxxAjvyjdyShjylCCq8dY1CVy888qYq22y88iDYY42j; unread={%22ub%22:%2269a951c0000000001a01dedd%22%2C%22ue%22:%2269ae5d0a000000001503bbee%22%2C%22uc%22:29}; webBuild=5.14.6; acw_tc=0a4aafd317732344948006520e8fa20695bdd0640fe349b73f7e05e9654e52; web_session=040069b81245c5fe21971021843b4bb4935648; id_token=VjEAAASQ9/j9PmY8hj9J0kRr1jE6QT0jbJ2EJBXMOG3SXzzUulDE2/ikIWYWXK7Il4BDESJWhztwd59iyuUwerj/WmkmqtaDv504ZRxuaFDY7YQUU1tJfNqSZEqhabqZNlNAKlMc; xsecappid=fe-login; loadts=1773234557651; websectiga=8886be45f388a1ee7bf611a69f3e174cae48f1ea02c0f8ec3256031b8be9c7ee; sec_poison_id=b0a9c277-fc6c-4e7c-a425-2e77bb0d7b72}',  # 请填写实际Cookie值
+        'value': 'abRequestId=7e3310df-011a-5d42-943e-fb44df97212c; a1=19c94f738easkxvhhq9y1cv91oxe8lxj119x1sxe130000383551; webId=f769b9d526c8861d4325189df5578d03; gid=yjSj4iW4WdyYyjSj4iWqYyMCd0MTChxxAjvyjdyShjylCCq8dY1CVy888qYq22y88iDYY42j; acw_tc=0a4ad06c17752222993131383e46ac7c3df01ed8095ff9afbcec8e77b7da40; ets=1775222303668; webBuild=6.3.0; xsecappid=xhs-pc-web; loadts=1775222303717; websectiga=59d3ef1e60c4aa37a7df3c23467bd46d7f1da0b1918cf335ee7f2e9e52ac04cf; sec_poison_id=e377477e-4d2e-4fb1-a55a-b933fc0f7d05; web_session=040069b81245c5fe21975af6fa3b4bd9da9dae; id_token=VjEAAIc2CjAsfidMvpDcTgLssqmRhkrb3zNUehIPWXaRDgKbHV8RtjqjbEiRVxtfFAmSIIDpnrbEVR7fhHBRB9uWTbcqIpksB5mpKCzkkL5bcZL+PydhgUBLOgoUbNyVa++2oU9d; unread={%22ub%22:%2269cfbdbe000000001a029771%22%2C%22ue%22:%2269ce6d37000000001b0230b5%22%2C%22uc%22:29}',  # 请填写实际Cookie值
         'account_info': '主账号'
     },
     # {
